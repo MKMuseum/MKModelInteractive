5a1d09046b99f23912c2061d28f7bb0facd40111
Mark R Dornan <128733408+MKMuseum@users.noreply.github.com>
2023-04-24 21:53:59 +0100

    Re-rooted

---
+++ MkModelMaster01/MkModelMaster01.ino
@@ -0,0 +1,246 @@
+
+/* MKModel Master PICO 
+*  Core 1 - Comms with touch sensor slave PICO (I2C) and DF12015 soundcard (serial)
+*  Core 2 - lamp control via TLC59711 (SPI) 
+*     (Based on the Adafruit TLC59711 PWM/LED driver library, authors: Limor Fried & Ladyada).
+* Parent sketches:
+*   MkModelLampManagerSPI03 (TLC59711 control via SPI)
+*   TouchPadTestI2Smaster01 (Pico - Pico comms via I2C)
+*   DF1201S_Test (DF12015 control via serial)
+* MRD 24/04/2023 ver 01
+*/
+
+//#include <SPI.h>
+#include <SPI.h>
+#include <Arduino.h>
+#include <DFRobot_DF1201S.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+// General constants
+#define HIGHLIGHT_TIMEOUT 60000
+
+// Constants and Globals for PICO - PICO I2C comms
+const char noPad = '@';
+
+// Definitions and global objects DF12015
+#define VOLUME 15
+#define TRACK 4
+SoftwareSerial DF1201SSerial(4, 5);  //RX  TX
+DFRobot_DF1201S DF1201S;
+
+// TLC9711 variables and constants
+#define DATAIN 16          // not used by TLC59711 breakout
+#define CHIPSELECT 17      // not used by TLC59711 breakout
+#define SPICLOCK 18        // ci on TLC59711 breakout
+#define DATAOUT 19         // di on TLC59711 breakout
+#define SPI_FREQ 14000000  // SPI frequency 
+#define LED_BRIGHT 0x7F
+#define RAMP_UP 255
+#define RAMP_DOWN -255
+#define MIN_PWM 0
+#define MAX_PWM 65535
+
+const int8_t numdrivers = 1;  // number of chained breakout boards
+const int8_t numChannels = 12 * numdrivers;
+uint8_t BCr = LED_BRIGHT;
+uint8_t BCg = LED_BRIGHT;
+uint8_t BCb = LED_BRIGHT;
+// Array of struct to hold current pwm and ramp direction for each channel
+struct channelState {
+  long pwm;
+  int ramp;
+} channelStates[numChannels];
+
+// TLC9711 functions
+// Initialise SPI for PI PICO
+void setupSPI() {
+  pinMode(SPICLOCK, OUTPUT);
+  pinMode(DATAOUT, OUTPUT);
+  SPI.setSCK(SPICLOCK);
+  SPI.setTX(DATAOUT);
+  delay(5);
+  SPI.begin();
+  delay(5);
+}
+
+// Write channel pwm info to TLC59711
+void tlcWrite() {
+  uint32_t command;
+
+  // Magic word for write
+  command = 0x25;
+
+  command <<= 5;
+  // OUTTMG = 1, EXTGCK = 0, TMGRST = 1, DSPRPT = 1, BLANK = 0 -> 0x16
+  command |= 0x16;
+
+  command <<= 7;
+  command |= BCr;
+
+  command <<= 7;
+  command |= BCg;
+
+  command <<= 7;
+  command |= BCb;
+
+  SPI.beginTransaction(SPISettings(SPI_FREQ, MSBFIRST, SPI_MODE0));
+  for (uint8_t n = 0; n < numdrivers; n++) {
+    SPI.transfer(command >> 24);
+    SPI.transfer(command >> 16);
+    SPI.transfer(command >> 8);
+    SPI.transfer(command);
+
+    // 12 channels per TLC59711
+    for (int8_t c = 11; c >= 0; c--) {
+      // 16 bits per channel, send MSB first
+      SPI.transfer(channelStates[n * 12 + c].pwm >> 8);
+      SPI.transfer(channelStates[n * 12 + c].pwm);
+    }
+  }
+
+  delayMicroseconds(200);
+  SPI.endTransaction();
+}
+
+// Set all channels to same pwm and ramp setting. 
+void setAllChannels(long pwm, int ramp) {
+  for(int channel = 0; channel < numChannels; channel++){
+    channelStates[channel].pwm = pwm;
+    channelStates[channel].ramp = ramp;  
+  }
+}
+
+// Fade lamps according to setting for each channel in the ramp element of the channel's state.
+void fadeLamps() {
+  for(int iter = 0; iter < 258; iter++) {
+    tlcWrite();
+    for(int channel = 0; channel < numChannels; channel++){
+        channelStates[channel].pwm = calculateNewPwm(channelStates[channel].pwm,channelStates[channel].ramp); 
+    }
+    delay(10);
+  }
+}
+
+// Identify a single channel to be faded up, mark others to be faded down. 
+void highlightLamp(uint32_t highlitChannel){
+  for(int channel = 0; channel < numChannels; channel++){
+    channelStates[channel].ramp = RAMP_DOWN;        
+  }
+  channelStates[highlitChannel].ramp = RAMP_UP;  
+}
+
+// Calculate the new pwm value based on current pwm and ramp values.
+long calculateNewPwm(long pwm, int ramp){
+  long newPwm = pwm + ramp;
+  if(newPwm < MIN_PWM) {
+    newPwm = MIN_PWM;
+  }
+  else if(newPwm > MAX_PWM){
+    newPwm = MAX_PWM;    
+  }
+  return newPwm;
+}
+
+// Test that a channel number is within the range of available channels.
+bool channelInRange(int chan) {
+  return (chan > -1 && chan < numChannels);
+}
+
+// Print current channel states
+void printChannelStates() {
+  for(int channel = 0; channel < numChannels; channel++){
+    Serial.print(channel);
+    Serial.print("   ");
+    Serial.print(channelStates[channel].pwm);
+    Serial.print("   ");
+    Serial.println(channelStates[channel].ramp); 
+  }
+}
+
+
+// Funtions for I2C PICO - PICO comms
+void initWire() {
+  Wire.setSDA(0);
+  Wire.setSCL(1);
+  Wire.begin();
+}
+
+// Read from the slave and print out
+char requestLastPadTouched() { 
+  char response = noPad;
+   
+  Wire.requestFrom(0x30,1);
+  while (Wire.available()) {
+    response = (char) Wire.read();
+  }
+  return response;
+}
+
+// Functions for DF1201
+void initDF1201(){
+  // Start SS and initialise DF12015 object
+  DF1201SSerial.begin(115200);
+  delay(1000);
+  while(!DF1201S.begin(DF1201SSerial)){
+    Serial.println("Init failed, please check the wire connection!");
+    delay(10000);
+  }
+  // Set volume to avoid 'music' announcement
+  DF1201S.setVol(0);
+  // Set Mode
+  DF1201S.switchFunction(DF1201S.MUSIC);
+  // Wait for the end of the prompt tone 
+  delay(2000);
+  DF1201S.setVol(VOLUME);
+  // Set playback mode to "repeat all" 
+  DF1201S.setPlayMode(DF1201S.SINGLE);
+}
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){}
+  // Start PICO - PICO comms
+  //initWire();
+  // Start the DF1201 sound player
+  initDF1201();
+  // Play the sound
+  DF1201S.playFileNum(TRACK);
+}
+
+void loop() {
+  uint32_t channelIn = 99;
+  while(Serial.available()) {       
+    channelIn = Serial.read() - 'A';
+    if(channelInRange(channelIn)) {
+      rp2040.fifo.push_nb(channelIn);
+    } 
+  }
+  delay(10);
+}
+
+void setup1() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  setupSPI();
+  delay(100);
+  setAllChannels(MIN_PWM, RAMP_UP);
+  fadeLamps();
+}
+
+void loop1() {
+  static long lastHighlightTS = 0;
+
+  if(rp2040.fifo.available() > 0){
+    uint32_t highlight = rp2040.fifo.pop();
+    highlightLamp(highlight);
+    fadeLamps();
+    lastHighlightTS = millis();
+  }
+  else if(lastHighlightTS > 0 && millis() > (lastHighlightTS + HIGHLIGHT_TIMEOUT)) {
+      setAllChannels(MIN_PWM, RAMP_UP);
+      fadeLamps();
+      lastHighlightTS = 0;
+  }
+  delay(20);
+}
---
+++ MkModelTouchReader01/MkModelTouchReader01.ino
@@ -0,0 +1,90 @@
+/* MKModel Touch Reader PICO 
+*  Core 1 - Touch pad polling and response to Master PICO I2C request 
+*  Core 2 - Unused
+* Parent sketches:
+*  MkModelTouchReader01
+* MRD 24/04/2023 ver 01
+*/
+#include <Wire.h>
+
+#define TOUCH_PIN 28
+#define MAX_TOUCH_HOLD 5000
+
+unsigned char noPad = '@';
+unsigned char lastTouch = noPad;
+// Polling sequence depends on boar conection which are not in strict PICO GPIO pin order
+const pin_size_t pinRef[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 14, 18, 19, 20, 21, 28, 27, 26, 22, 17, 18, 12, 13 };
+const int padCount = sizeof(pinRef) / sizeof(pin_size_t);
+
+void setup() {
+  //Serial.begin(9600);
+  //while(!Serial){}
+  //Serial.println("Initialising");
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Set touchpad GPIO pin modes
+  initTouchPads();
+  // initialaise I2C communincations
+  initWire();
+  //Serial.println("Ready");
+}
+
+void loop() {
+  readTouchPads();
+}
+
+void initTouchPads(){
+  // initialise touch pad GPIO pins
+  for (int index = 0; index < padCount; index++) {
+    pinMode(pinRef[index], INPUT);
+    //Serial.println(pinRef[index]);
+  }
+}
+
+void initWire(){
+   // initialise the I2C comms
+  Wire.setSDA(0);
+  Wire.setSCL(1);
+  Wire.begin(0x30);
+  Wire.onReceive(recv);
+  Wire.onRequest(req);
+  delay(100);
+}
+
+void readTouchPads() {
+  for (int index = 0; index < padCount; index++) {
+    long touchRelease = 0;
+    if (digitalRead(pinRef[index]) == HIGH) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      touchRelease = millis();
+      while (digitalRead(pinRef[index]) == HIGH) {
+        if (millis() > touchRelease + MAX_TOUCH_HOLD) {
+          break;
+        }
+      }
+      digitalWrite(LED_BUILTIN, LOW);
+      recordTouch(index);
+      break;
+    }
+  }
+}
+
+void recordTouch(int padIndex) {
+  // record 'A' for index 0, 'X' for index 23 etc.
+  // recorded value refernces PCB touchpad connector order not PICO GPIO pin number  
+  lastTouch = padIndex + 'A';
+  //Serial.println(lastTouch);
+}
+
+// Called when the I2C slave gets written to
+void recv(int len) {
+  // Just consume anything recieved for now
+  for (int i = 0; i < len; i++) {
+    Wire.read();
+  }
+}
+
+// Called when the I2C slave is read from
+void req() {
+  Wire.write((byte)lastTouch);
+  lastTouch = noPad;
+}
